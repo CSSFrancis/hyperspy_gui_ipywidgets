4dff8475e4d8b14dd710d593e32b0fd0941d552e
Carter Francis <csfrancis@wisc.edu>
2023-07-23 15:51:37 -0500

    Split spectrospy functions to seperate gui widgets

--- hyperspy_gui_ipywidgets/model.py
+++ hyperspy_gui_ipywidgets/model.py
@@ -187,40 +187,6 @@ def get_model_widget(obj, **kwargs):
         "wdict": wdict
     }
 
-
-@add_display_arg
-def get_eelscl_widget(obj, **kwargs):
-    """Create ipywidgets for the EELSCLEdge component.
-
-    """
-    wdict = {}
-    active = Checkbox(description='active', value=obj.active)
-    fine_structure = Checkbox(description='Fine structure',
-                              value=obj.fine_structure_active)
-    fs_smoothing = FloatSlider(description='Fine structure smoothing',
-                               min=0, max=1, step=0.001,
-                               value=obj.fine_structure_smoothing)
-    link((obj, "active"), (active, "value"))
-    link((obj, "fine_structure_active"),
-         (fine_structure, "value"))
-    link((obj, "fine_structure_smoothing"),
-         (fs_smoothing, "value"))
-    container = VBox([active, fine_structure, fs_smoothing])
-    wdict["active"] = active
-    wdict["fine_structure"] = fine_structure
-    wdict["fs_smoothing"] = fs_smoothing
-    for parameter in [obj.intensity, obj.effective_angle,
-                      obj.onset_energy]:
-        pdict = parameter.gui(
-            toolkit="ipywidgets", display=False)["ipywidgets"]
-        container.children += pdict["widget"],
-        wdict["parameter_{}".format(parameter.name)] = pdict["wdict"]
-    return {
-        "widget": container,
-        "wdict": wdict,
-    }
-
-
 @add_display_arg
 def get_scalable_fixed_patter_widget(obj, **kwargs):
     cdict = get_component_widget(obj, display=False)
